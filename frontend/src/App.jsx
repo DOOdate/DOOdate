ae910bf56d22db510acdf6a38e4714afc19b0fe7
Jake Hollmann <jholl015@uottawa.ca>
2025-10-17 19:53:33 -0400

    add dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import React from 'react'
 import Login from './login.jsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import Typography from '@mui/material/Typography'
+import { bgcolor } from "@mui/system";
+import Box from '@mui/material/Box';
 
 function Login(){
     return (
-        <Typography variant="body1" color="initial">test</Typography>
+        <Box
+        sx={{
+            display: 'flex',
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: 'background.default',
+            color: 'text.primary',
+        }}
+        >
+            <Typography variant="h1">DooDate</Typography>
+        </Box>
     )
 }
 export default Login
