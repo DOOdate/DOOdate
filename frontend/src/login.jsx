52303991d3da71c5d44263f46957f33a762854cd
Jake Hollmann <jholl015@uottawa.ca>
2025-10-17 17:09:46 -0400

    add router stuff

---
+++ frontend/src/login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Typography from '@mui/material/Typography'
+
+function Login(){
+    return (
+        <Typography variant="body1" color="initial">test</Typography>
+    )
+}
+export default Login
