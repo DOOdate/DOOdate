6d3ef1c0d54080ee46af0898f15d25ab4405bf39
Jake Hollmann <jholl015@uottawa.ca>
2025-10-17 17:18:21 -0400

    clean up code from template, remove css files as styling should be done with MUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Login from './login.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
