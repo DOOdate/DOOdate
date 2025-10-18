c768f7b85947a11ac36876837f2d6723b86bf4e7
Cameron Labelle <labelle.cam3@gmail.com>
2025-10-18 19:02:09 -0400

    Some progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,10 @@ import React from 'react'
 import Login from './login.jsx'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Navbar } from "./components/Navbar.jsx";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material"
+import CalendarIcon from "./assets/calendar-icon.svg"
+import AddIcon from "./assets/plus-icon.svg"
+import SettingsIcon from "./assets/settings-icon.svg"
 
 const theme = createTheme({
   colorSchemes: {
@@ -13,15 +16,28 @@ const theme = createTheme({
 });
 
 function App() {
+  const [value, setValue] = useState(0);
+  const pages = ['/home', '/addsyllabus', '/settings']
   return (
     <ThemeProvider theme={theme}>
-      <Navbar />
       <CssBaseline />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
+      <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+            navigate(pages[newValue]);
+          }}
+        >
+          <BottomNavigationAction label="Calendar" icon={<img src={CalendarIcon} />} />
+          <BottomNavigationAction label="Add Class" icon={<img src={AddIcon} />} />
+          <BottomNavigationAction label="Settings" icon={<img src={SettingsIcon} />} />
+        </BottomNavigation>
     </ThemeProvider>
   )
 }
