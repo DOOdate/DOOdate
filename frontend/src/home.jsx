b1485a836a4f5b1b026376ba7c81668ac2ad9f14
Cameron Labelle <labelle.cam3@gmail.com>
2025-10-18 17:16:00 -0400

    Setup jsx files for each page

---
+++ frontend/src/home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Typography from '@mui/material/Typography'
+import { bgcolor } from "@mui/system";
+import Box from '@mui/material/Box';
+
+function Home(){
+    return (
+        <Box
+        sx={{
+            display: 'flex',
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: 'background.default',
+            color: 'text.primary',
+        }}
+        >
+            <Typography variant="h1">DooDate</Typography>
+        </Box>
+    )
+}
+export default Home
