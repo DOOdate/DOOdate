c42360d15f68c0732651b80f6d3f5159dc77ea75
Cameron Labelle <labelle.cam3@gmail.com>
2025-10-21 23:21:52 -0400

    Revert change to default page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import React from 'react'
 import Login from './login.jsx'
-import Home from './home.jsx'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -34,7 +33,7 @@ function App() {
       <CssBaseline />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
