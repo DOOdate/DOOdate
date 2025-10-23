2021ee1c396c0a8b6716f6d84391c7a4a0d16617
Cameron Labelle <labelle.cam3@gmail.com>
2025-10-23 14:47:36 -0400

    Fix nav button coloring in light mode

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,11 +6,13 @@ import AddIcon from '../assets/plus-icon.svg';
 import SettingsIcon from '../assets/settings-icon.svg';
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
+import { useColorScheme  } from "@mui/material/styles";
 
 function Navbar() {
     const [value, setValue] = useState(0);
     const pages = ['../home', '../addsyllabus', '../settings'];
     const navigate = useNavigate();
+    const { mode, setMode } = useColorScheme();
     return (
         <BottomNavigation
           showLabels
@@ -21,9 +23,9 @@ function Navbar() {
             navigate(pages[newValue]);
           }}
         >
-          <BottomNavigationAction label="Calendar" icon={<img src={CalendarIcon} class="navbar-icon" />} />
-          <BottomNavigationAction label="Add Class" icon={<img src={AddIcon} class="navbar-icon" />} />
-          <BottomNavigationAction label="Settings" icon={<img src={SettingsIcon} class="navbar-icon" />} />
+          <BottomNavigationAction label="Calendar" icon={<img src={CalendarIcon} className="navbar-icon" style={{filter: `invert(${mode === "dark" ? 1 : 0})`}} />} />
+          <BottomNavigationAction label="Add Class" icon={<img src={AddIcon} className="navbar-icon" style={{filter: `invert(${mode === "dark" ? 1 : 0})`}}/>} />
+          <BottomNavigationAction label="Settings" icon={<img src={SettingsIcon} className="navbar-icon" style={{filter: `invert(${mode === "dark" ? 1 : 0})`}} />} />
         </BottomNavigation>
     )   
 }
