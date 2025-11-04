877a180d031c9538788b09de5945888311d0d3de
Cameron Labelle <labelle.cam3@gmail.com>
2025-11-04 11:24:38 -0500

    Initial commit

---
+++ backend/core/services/pdf_db.py
@@ -0,0 +1,28 @@
+﻿from django.core.files.uploadedfile import UploadedFile
+import hashlib
+
+
+class PDFSearcher:
+    def __init__(self):
+
+
+class PDFHasher:
+    @staticmethod
+    def hash(pdf : UploadedFile) -> str:
+        hasher = hashlib.md5() # md5 for fast hashing but low security (should be fine)
+        for chunk in pdf.chunks():
+            hasher.update(chunk)
+        return hasher.hexdigest()
+
+    @staticmethod
+    def equals(a, b) -> bool:
+        # Need to compare an UploadedFile type to however we store the file in the database
+
+        # Probability of hash collision is astronomically low, but can be induced by malicious users. Is this something we should worry about?
+
+        return False
+
+    @staticmethod
+    def exists() -> bool:
+        # Compare the UploadedFile with all collisions (call the equals method on each collision)
+        return False
