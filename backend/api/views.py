d76043d7a2125bf39f3e21f5a8ac7549887163d4
Cameron Labelle <labelle.cam3@gmail.com>
2025-11-05 09:51:34 -0500

    Refactor services to top-level methods

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from parser.models import Test
 from .serializers import TestSerializer
+from ..core.services import pdf_db, syllabus_parser
+from django.core.files.uploadedfile import UploadedFile
 
 
 @api_view(['GET'])
@@ -22,3 +24,15 @@ def addTest(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['POST'])
+def uploadSyllabus(request):
+    file = request.FILES.get('myFile')
+    if file:
+        cached = pdf_db.find(file)
+        if cached is None:
+            res = syllabus_parser.parse(file)
+            # Add res to the database
+            # Serialize res as JSON
+            return Response(res)
+        return Response(cached)
--- backend/core/services/pdf_db.py
+++ backend/core/services/pdf_db.py
@@ -4,32 +4,28 @@ from django.core.files.uploadedfile import UploadedFile
 import hashlib
 from parser.models import Syllabus, Course
 
-class PDFSearcher:
-    @staticmethod
-    def find(pdf : UploadedFile) -> Course | None:
-        file_hash = PDFHasher.hash(pdf)
-        matches = Syllabus.objects.filter(hash=file_hash)
-        if len(matches) == 0: return None
-        # Assume for now that there is no hash collision
-        return matches[0]
 
-class PDFHasher:
-    @staticmethod
-    def hash(pdf : UploadedFile) -> str:
-        hasher = hashlib.md5() # md5 for fast hashing but low security (should be fine)
-        for chunk in pdf.chunks():
-            hasher.update(chunk)
-        return hasher.hexdigest()
+def find(pdf : UploadedFile) -> Course | None:
+    file_hash = hash(pdf)
+    matches = Syllabus.objects.filter(hash=file_hash)
+    if len(matches) == 0: return None
+    # Assume for now that there is no hash collision
+    return matches[0]
 
-    @staticmethod
-    def equals(a, b) -> bool:
-        # Need to compare an UploadedFile type to however we store the file in the database
 
-        # Probability of hash collision is astronomically low, but can be induced by malicious users. Is this something we should worry about?
+def hash(pdf : UploadedFile) -> str:
+    hasher = hashlib.md5() # md5 for fast hashing but low security (should be fine)
+    for chunk in pdf.chunks():
+        hasher.update(chunk)
+    return hasher.hexdigest()
 
-        return a == b
+def equals(a, b) -> bool:
+    # Need to compare an UploadedFile type to however we store the file in the database
 
-    @staticmethod
-    def exists() -> bool:
-        # Compare the UploadedFile with all collisions (call the equals method on each collision)
-        return False
+    # Probability of hash collision is astronomically low, but can be induced by malicious users. Is this something we should worry about?
+
+    return a == b
+
+def exists() -> bool:
+    # Compare the UploadedFile with all collisions (call the equals method on each collision)
+    return False
