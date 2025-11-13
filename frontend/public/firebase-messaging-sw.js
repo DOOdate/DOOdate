01ebfa53b9b6a8828e056ada5051a43b51014641
Jake Hollmann <jholl015@uottawa.ca>
2025-11-13 18:29:17 -0500

    remove comments from firebase code that are not useful (were there from the sample code I used)

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,9 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
   apiKey: "AIzaSyD7dLPtksZgzNzatRTa8HLlRUqTmH-mFdU",
   authDomain: "doodate-29075.firebaseapp.com",
@@ -13,18 +10,13 @@ const firebaseConfig = {
   appId: "1:419136523064:web:db50421b85816e22634fcd"
 };
 
-// Initialize Firebase app instance for the service worker
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage(messaging, (payload) => {
   console.log(
     '[firebase-messaging-sw.js] Received background message ',
     payload,
   );
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
