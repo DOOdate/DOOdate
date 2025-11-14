118f9d4fc55c05961c8869196059e27828f218b2
Jake Hollmann <jholl015@uottawa.ca>
2025-11-14 15:32:13 -0500

    add notify_user function and test it, sends notification from backend to frontend properly

---
+++ backend/core/services/schedule_notification.py
@@ -0,0 +1,28 @@
+from core.services.setup_django import setup_django
+setup_django()
+from parser.models import User
+import firebase_admin
+from firebase_admin import messaging, credentials
+
+cred = credentials.Certificate("./doodate-firebase-adminsdk.json") # pls nobody mess with my account
+firebase_admin.initialize_app(cred)
+
+
+def notify_user(user, title, body): # TESTED WORKING
+    """
+    Sends a notification to a user with the given title and body.
+    """
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+        ),
+        token=user.notification_token,
+    )
+    response = messaging.send(message)
+
+
+
+def schedule_notification(user, time, course_code, assignment_name):
+    pass #TODO: implement notification scheduling (notify 2 days before, 1 day before, and on the day)
+# using django-q
