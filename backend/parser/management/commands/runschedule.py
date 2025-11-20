bdc94e418ea2eb861c2f946e7b062b3c3a09e0e3
Jake Hollmann <jholl015@uottawa.ca>
2025-11-20 17:47:09 -0500

    properly format the runschedule command

--- backend/parser/management/commands/runschedule.py
+++ backend/parser/management/commands/runschedule.py
@@ -8,18 +8,22 @@ from datetime import timedelta
 # CURRENTLY, WHEN ADDING A COURSE, USER IS NULL
 # DOES WORK IF YOU MANUALLY SET A USER FOR A COURSE IN THE DB
 
-while True:
-    users = User.objects.all()
-    for user in users: # loop through all users
-        courses = Course.objects.filter(user=user)
-        for course in courses: # loop through all courses for that user
-            deadlines = Deadline.objects.filter(course=course)
-            for deadline in deadlines: # loop through all deadlines for that course
-                now = timezone.now()
-                if now - timedelta(days=2) <= deadline.due_date <= now - timedelta(days=2, minutes=-1): # if deadline is in 2 days (ignores seconds)
-                    notify_user(user, course.course_code, deadline.title + " is due in 2 days")
-                if now - timedelta(days=1) <= deadline.due_date <= now - timedelta(days=1, minutes=-1): # if deadline is in 1 day (ignores seconds)
-                    notify_user(user, course.course_code, deadline.title + " is due in 1 days")
-                if now - timedelta(hours=6) <= deadline.due_date <= now - timedelta(hours=6, minutes=-1): # if deadline is in 6 hours (ignores seconds)
-                    notify_user(user, course.course_code, deadline.title + " is due in 6 hours")
-    sleep(60)  # wait 60 seconds (1 min)
+class Command(BaseCommand):
+    help = 'Notifies users when the deadlines are approaching, must run for full app functionality'
+
+    def handle(self, *args, **options):
+        while True:
+            users = User.objects.all()
+            for user in users: # loop through all users
+                courses = Course.objects.filter(user=user)
+                for course in courses: # loop through all courses for that user
+                    deadlines = Deadline.objects.filter(course=course)
+                    for deadline in deadlines: # loop through all deadlines for that course
+                        now = timezone.now()
+                        if now - timedelta(days=2) <= deadline.due_date <= now - timedelta(days=2, minutes=-1): # if deadline is in 2 days (ignores seconds)
+                            notify_user(user, course.course_code, deadline.title + " is due in 2 days")
+                        if now - timedelta(days=1) <= deadline.due_date <= now - timedelta(days=1, minutes=-1): # if deadline is in 1 day (ignores seconds)
+                            notify_user(user, course.course_code, deadline.title + " is due in 1 days")
+                        if now - timedelta(hours=6) <= deadline.due_date <= now - timedelta(hours=6, minutes=-1): # if deadline is in 6 hours (ignores seconds)
+                            notify_user(user, course.course_code, deadline.title + " is due in 6 hours")
+            sleep(60)  # wait 60 seconds (1 min)
