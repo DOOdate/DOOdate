8a529fe535e00dd180ef1ecaa4e5afdcb4aae93b
Cameron Labelle <labelle.cam3@gmail.com>
2025-11-24 15:14:29 -0500

    Fix issues with merge

--- backend/parser/models.py
+++ backend/parser/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    pass
+    notification_token = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.notification_token
 
 class Course(models.Model):
     course_code = models.CharField(max_length=7, default='') # I think they can only be 7?
